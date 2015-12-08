b0ef2adb519b0887423be141af4a3378f42e47f9
mboehme <hawkie@web.de>
2015-12-08 11:48:03 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,22 +6,41 @@ if [ $(docker images | grep -c "CREATED") -eq 0 ]; then
   echo "Run boot2docker to set up docker environment."
   exit 1
 fi
-if [ $(docker images | grep -c "mboehme/corebench") -eq 0 ]; then
-  echo "Execute 'docker build -t mboehme/corebench .' or tag the image as 'mboehme/corebench'".
+if [ $(docker images | grep -c "mboehme/corebench*") -eq 0 ]; then
+  echo "Execute 'docker pull mboehme/corebench' and/or 'docker pull mboehme/corebenchx'"
+  echo "Or execute 'docker build -t mboehme/corebench .' and/or 'docker build -t mboehme/corebenchx .'"
+  echo "Or tag an image as mboehme/corebench or mboehme/corebenchx"
   exit 1
 fi
-if [ $(docker ps | grep -c "mboehme/corebench") -ne 0 ]; then
-  echo "An instance of 'mboehme/corebench' is already running ($(docker ps | grep mboehme/corebench | cut -c-12))"
+if [ $(docker images | grep -c "mboehme/corebench*") -gt 1 ] && [ -z "$1" ]; then
+  echo "Execute $0 [corebench|corebenchx] to specify which instance to run."
+  exit 1
+fi
+
+corebench=$(if [ -z "$1" ]; then echo $(docker images | grep "mboehme/corebench*" | cut -d" " -f1); else echo "$1")
+
+if [ $(docker ps | grep -c "mboehme/$corebench ") -ne 0 ]; then
+  echo "An instance of 'mboehme/$corebench' is already running ($(docker ps | grep "mboehme/$corebench " | cut -c-12))"
   echo "Connecting .."
-  docker exec -it $(docker ps | grep mboehme/corebench | cut -c-12) bash
+  docker exec -it $(docker ps | grep "mboehme/$corebench " | cut -c-12) bash
+  exit 0
+fi
+
+if [[ "$corebench" == "corebench" ]]; then
+  printf "Container (find, grep, make): "
+  docker run -dt --name corebench0 -v $(pwd):/shared -p 5900:5900 --dns 8.8.8.8 --dns 8.8.4.4 mboehme/corebench | cut -c-12
+  echo "Now use VNCViewer to access $(boot2docker ip):5900 (password: $(cat password.txt))"
+elif [[ "$corebench" == "corebenchx" ]]; then
+  printf "Container (coreutils): "
+  docker run -dt --name corebenchx0 -v $(pwd):/shared -p 5901:5900 --dns 8.8.8.8 --dns 8.8.4.4 mboehme/corebenchx | cut -c-12
+  echo "Now use VNCViewer to access $(boot2docker ip):5901 (password: $(cat password.txt))"
+else
+  echo "Unknown: $corebench. Please use 'corebench' or 'corebenchx'."
   exit 1
 fi
-printf "Container: "
-docker run -dt --name corebench0 -v $(pwd):/shared -p 5900:5900 --dns 8.8.8.8 --dns 8.8.4.4 mboehme/corebench | cut -c-12
-echo Now use a VNCViewer to access $(boot2docker ip):5900 (password: $(cat password.txt))
-echo 
-echo Note: Once the container removed or broken, any temporary data will be lost!
+echo
+echo Note: Once the container is removed or broken, any temporary data will be lost!
 echo Use the '/shared'-folder for scripts and data which you would like to keep.  
 echo
 echo Connecting..
-docker exec -it $(docker ps | grep mboehme/corebench | cut -c-12) bash
+docker exec -it $(docker ps | grep "mboehme/$corebench " | cut -c-12) bash
