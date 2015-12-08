8f8f26beee558f8a2ed2409352b8e060c98a7b15
mboehme <hawkie@web.de>
2015-12-08 10:08:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+if [ -z "$(which docker)" ]; then
+  echo "Install docker from http://docs.docker.com/engine/installation/"
+  exit 1
+fi
 if [ $(docker images | grep -c "CREATED") -eq 0 ]; then
   echo "Run boot2docker to set up docker environment."
   exit 1
@@ -16,7 +20,6 @@ printf "Container: "
 docker run -dt --name corebench0 -v $(pwd):/shared -p 5900:5900 --dns 8.8.8.8 --dns 8.8.4.4 mboehme/corebench | cut -c-12
 echo Now use a VNCViewer to access $(boot2docker ip):5900 (password: $(cat password.txt))
 echo 
-echo
 echo Note: Once the container removed or broken, any temporary data will be lost!
 echo Use the '/shared'-folder for scripts and data which you would like to keep.  
 echo
